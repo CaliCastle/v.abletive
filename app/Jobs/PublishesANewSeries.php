c76f3ef9f832cb726c88147dffd36e3436029153
= <cali@calicastle.com>
2016-04-04 13:57:03 +0800

    Add job for publishing a new series and code clean up

---
+++ app/Jobs/PublishesANewSeries.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Jobs;
+
+use App\User;
+use App\Series;
+use App\Jobs\Job;
+use Illuminate\Support\Facades\Mail;
+use App\Http\Requests\SeriesRequest;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class PublishesANewSeries extends Job implements ShouldQueue {
+
+    use InteractsWithQueue, SerializesModels;
+    /**
+     * @var Series
+     */
+    private $series;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @param SeriesRequest $request
+     */
+    public function handle(SeriesRequest $request)
+    {
+        $series = Series::create($request->all());
+        if (!$series)
+            return redirect()->back()->withInput($request->all());
+        $series->skills()->attach($request->input('skills'));
+
+        $this->series = $series;
+
+        // Send email notifications
+        $this->sendEmail();
+    }
+
+    /**
+     * Send email to its subscribers
+     *
+     * @return $this
+     */
+    private function sendEmail()
+    {
+        foreach (User::all() as $user) {
+            if ($user->subscribed() && !is_null($user->email) && $user->email != "") {
+                Mail::queue('emails.new_series', ['user' => $user, 'series' => $this->series], function ($m) use ($user) {
+                    $m->from('cali@calicastle.com', config('app.site.title'));
+                    $m->to($user->email)->subject('您订阅的课程更新啦!');
+                });
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ resources/views/emails/new_series.blade.php
@@ -0,0 +1,11 @@
+@extends('emails.base')
+
+@section('title', "全新教程系列《".$series->title."》发布了")
+
+@section('content')
+    <h3>《{{ $series->title }}》系列课程发布啦!</h3>
+    <b><a href="{{ $series->link() }}" target="_blank"><img src="{{ $series->thumbnail }}" alt="{{ $series->title }}"></a></b>
+    <p>嗨, <strong>{{ $user->display_name }}</strong>! - </p>
+    <h4>系列课程介绍: {!! $series->description !!}</h4>
+    <p>该课程难度等级为: "{{ trans('lessons.difficulty.' . $series->difficulty) }}"</p>
+@stop
